7dee6ba1ba0ab9db450e0b13d3d2fd28012d4985
ngochai68 <ngochai22012003@gmail.com>
2023-06-04 23:15:29 +0700

    Message describing the changes

--- js/movies.js
+++ js/movies.js
@@ -1,8 +1,7 @@
 async function getMovies() {
   try {
     const response = await fetch('http://localhost:3000/movies');
-    const movies = await response.json();
-    return movies;
+    return response.json();
   } catch (error) {
     console.error('Error:', error);
   }
@@ -11,39 +10,40 @@ async function getMovies() {
 async function renderMovies() {
   const movies = await getMovies();
   const sliderMove = document.querySelector('.light-slider-container .slider-content .slider-move');
+  const fragment = document.createDocumentFragment();
 
   movies.forEach(movie => {
-    const html = `<div class="slide-item-wrap">
-                    <div class="lightSlider">
+    const slideItemWrap = document.createElement('div');
+    slideItemWrap.classList.add('slide-item-wrap');
+    
+    const html = `<div class="lightSlider">
                     <div class="plist-img-wrap">
-                        <div>
+                      <div>
                         <a href="#">
-                            <div class="pic-box">
+                          <div class="pic-box">
                             <span>
-                                <img src="${movie.img_1_url}" alt="" />
+                              <img src="${movie.img_1_url}" alt="" />
                             </span>
                             <div class="update-info-layer">
-                                <div class="update-info-mask">Cập nhật tập 28</div>
+                              <div class="update-info-mask">Cập nhật tập 28</div>
                             </div>
-                            </div>
+                          </div>
                         </a>
-                        </div>
-                        <a href="#">
+                      </div>
+                      <a href="#">
                         <div class="text-box">
-                            <p class="title">${movie.name}</p>
+                          <p class="title">${movie.name}</p>
                         </div>
-                        </a>
+                      </a>
                     </div>
-                    </div>
-                </div>`;
-
-    const wrapper = document.createElement('div');
-    wrapper.innerHTML = html;
-
-    const slideItemWrap = wrapper.firstChild;
-
-    sliderMove.appendChild(slideItemWrap);
+                  </div>`;
+    
+    slideItemWrap.innerHTML = html;
+    fragment.appendChild(slideItemWrap);
   });
+
+
+  sliderMove.appendChild(fragment);
 }
 
 renderMovies();
